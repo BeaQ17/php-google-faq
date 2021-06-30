8a986cd62603557d13e37c19df3a8adf5290013d
BeaQ17 <beaq235@gmail.com>
2021-06-30 21:10:33 +0200

    q&a

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+$faq = [
+    [
+        "question" => "Lorem ipsum dolor?",
+        "answer" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p>"
+    ],
+    [
+        "question" => "Sit amet?",
+        "answer" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p>"
+    ],
+    [
+        "question" => "Consectetur adipisicing elit?",
+        "answer" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p>"
+    ],
+    [
+        "question" => "Officiis lorem ipsum?",
+        "answer" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Porro, officiis.</p>"
+    ]
+]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+</head>
+<body>
+    
+</body>
+</html>
